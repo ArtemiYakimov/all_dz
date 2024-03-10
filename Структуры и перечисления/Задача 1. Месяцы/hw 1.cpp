1a131d2f1ba0065566ba7738ab8cd237d5df396d
Artemi Yakimov <artemiyakimov246@gmail.com>
2024-03-10 21:10:44 +1100

    ficks

--- Структуры и перечисления/Задача 1. Месяцы/hw 1.cpp
+++ Структуры и перечисления/Задача 1. Месяцы/hw 1.cpp
@@ -2,50 +2,52 @@
 #include <Windows.h>
 
 using namespace std;
-
-enum mounts {
+    
+    enum class mounts {
     january = 1,
-    february = 2,
-    march = 3,
-    april = 4,
-    may = 5,
-    june = 6,
-    july = 7,
-    august = 8,
-    september = 9,
-    october = 10,
-    november = 11,
-    december = 12
-};
+    february, 
+    march,
+    april,
+    may,
+    june,
+    july,
+    august,
+    september,
+    october,
+    november,
+    december
+    };
 
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     bool flag = true;
     int a = 0;
+    
     do {
-        cout << "Введите номер месяца: ";
+        std::cout << "Введите номер месяца: ";
         cin >> a;
+        mounts mount = static_cast<mounts>(a);
         if (a > 0) {
-            if (a == mounts::january) cout << "Январь" << endl;
-            else if (a == mounts::february) cout << "Февраль" << endl;
-            else if (a == mounts::march) cout << "Март" << endl;
-            else if (a == mounts::april) cout << "Апрель" << endl;
-            else if (a == mounts::may) cout << "Май" << endl;
-            else if (a == mounts::june) cout << "Июнь" << endl;
-            else if (a == mounts::july) cout << "Июль" << endl;
-            else if (a == mounts::august) cout << "Август" << endl;
-            else if (a == mounts::september) cout << "Сентябрь" << endl;
-            else if (a == mounts::october) cout << "Октябрь" << endl;
-            else if (a == mounts::november) cout << "Ноябрь" << endl;
-            else if (a == mounts::december) cout << "Декабрь" << endl;
-            else cout << "Неправильный номер!" << endl;
+            if (mount == mounts::january) std::cout << "Январь" << endl;
+            else if (mount == mounts::february) std::cout << "Февраль" << endl;
+            else if (mount == mounts::march) std::cout << "Март" << endl;
+            else if (mount == mounts::april) std::cout << "Апрель" << endl;
+            else if (mount == mounts::may) std::cout << "Май" << endl;
+            else if (mount == mounts::june) std::cout << "Июнь" << endl;
+            else if (mount == mounts::july) std::cout << "Июль" << endl;
+            else if (mount == mounts::august) std::cout << "Август" << endl;
+            else if (mount == mounts::september) std::cout << "Сентябрь" << endl;
+            else if (mount == mounts::october) std::cout << "Октябрь" << endl;
+            else if (mount == mounts::november) std::cout << "Ноябрь" << endl;
+            else if (mount == mounts::december) std::cout << "Декабрь" << endl;
+            else std::cout << "Неправильный номер!" << endl;
         }
         else {
             flag = false;
         }
     } while (flag);
-    cout << "До свидания" << endl;
-    
+    std::cout << "До свидания" << endl;
+   
         return 0;
 }
