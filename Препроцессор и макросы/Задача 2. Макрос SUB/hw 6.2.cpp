3ec5d11662b94d9ca2234d234ea4d3f44eaab273
Artemi Yakimov <artemiyakimov246@gmail.com>
2024-03-14 18:33:06 +1100

    Процессор_и_макросы

---
+++ Препроцессор и макросы/Задача 2. Макрос SUB/hw 6.2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <Windows.h>
+#define SUB(a, c) ((a) - (c)) 
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	cout << SUB(a, b) << endl;
+	cout << SUB(a, b) * c << endl;
+	cout << SUB(a, b + c) * c << endl;
+	
+    return 0;
+}
