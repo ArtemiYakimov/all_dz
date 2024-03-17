09d716fdb95a618ed7c8ad44f8067ef7b8347dc9
Artemi Yakimov <artemiyakimov246@gmail.com>
2024-03-17 11:14:12 +1100

    operator

---
+++ Перегрузка операторов/Задача 1. Сравнения в дробях/hw 8.1.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+	int arr[2] = { 0, 0 };
+	void number(Fraction drob) {
+		arr[0] = numerator_ * drob.denominator_;
+		arr[1] = denominator_ * drob.numerator_;
+	}
+public:
+	Fraction(int numerator, int denominator) {
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+
+	bool operator == (Fraction drob) {
+		number(drob);
+		return arr[0] == arr[1];
+	};
+
+	bool operator != (Fraction drob) {
+		return !(*this == drob);
+	};
+
+	bool operator < (Fraction drob) {
+		number(drob);
+		return arr[0] < arr[1];
+	};
+
+	bool operator > (Fraction drob) {
+		number(drob);
+		return arr[0] > arr[1];
+	};
+
+	bool operator <= (Fraction drob) {
+		number(drob);
+		return !(arr[0] > arr[1]);
+	};
+
+	bool operator >= (Fraction drob) {
+		number(drob);
+		return !(arr[0] < arr[1]);
+	};
+};
+
+
+int main(){
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	Fraction f1(4, 3);
+	Fraction f2(6, 11);
+
+	cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	return 0;
+}
---
+++ Перегрузка операторов/Задача 2. Остальные операции с дробями/hw 8.2.cpp
@@ -0,0 +1,124 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+class Fraction {
+private:
+	int numerator_;
+	int denominator_;
+	int arr[2] = { 0, 0 };
+	void number(Fraction drob) {
+		arr[0] = numerator_ * drob.denominator_;
+		arr[1] = denominator_ * drob.numerator_;
+	}
+	int mini(int a, int b) {
+		if (a == b) {
+			return a;
+		}
+		a > b ? mini(a - b, b) : mini(a, b - a);
+	}
+
+	Fraction non() {
+		int a = numerator_ > 0 ? numerator_ : (numerator_ * (-1));
+		int b = denominator_ > 0 ? denominator_ : (denominator_ * (-1));
+		int c = mini(a, b);
+		return Fraction(numerator_ / c, denominator_ / c);
+	}
+public:
+	Fraction(int numerator, int denominator) {
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+
+	Fraction operator + (Fraction drob) {
+		int q = numerator_ * drob.denominator_ + drob.numerator_ * denominator_;
+		int z = denominator_ * drob.denominator_;
+		return Fraction(q, z).non();
+	};
+
+	Fraction operator - (Fraction drob) {
+		int q = numerator_ * drob.denominator_ - drob.numerator_ * denominator_;
+		int z = denominator_ * drob.denominator_;
+		return Fraction(q, z).non();
+	};
+
+	Fraction operator * (Fraction drob) {
+		int q = numerator_ * drob.numerator_;
+		int z = denominator_ * drob.denominator_;
+		return Fraction(q, z).non();
+	};
+
+	Fraction operator / (Fraction drob) {
+		int q = numerator_ * drob.denominator_;
+		int z = denominator_ * drob.numerator_;
+		return Fraction(q, z).non();
+	};
+
+	Fraction& operator ++ () {
+		numerator_ = numerator_ + denominator_;
+		return *this;
+	};
+
+	Fraction& operator ++ (int) {
+		Fraction drob = *this;
+		(*this).numerator_ = numerator_ + denominator_;
+		return drob;
+	};
+
+	Fraction& operator -- () {
+		numerator_ = numerator_ - denominator_;
+		return *this;
+	};
+
+	Fraction& operator -- (int) {
+		Fraction drob = *this;
+		(*this).numerator_ = numerator_ - denominator_;
+		return drob;
+	};
+
+	int get_num() {
+		return this->numerator_;
+	};
+	int get_den() {
+		return this->denominator_;
+	};
+};
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int a, b, c, d;
+
+	cout << "Введите числитель дроби 1: ";
+	cin >> a;
+	cout << "Введите знаменатель дроби 1: ";
+	cin >> b;
+	cout << "Введите числитель дроби 2: ";
+	cin >> c;
+	cout << "Введите знаменатель дроби 2: ";
+	cin >> d;
+
+	Fraction number(a, b);
+	Fraction numberok(c, d);
+	Fraction x = number + numberok;
+	cout << a << "/" << b << " + " << c << "/" << d << " = " << x.get_num() << "/" << x.get_den() << endl;
+    x = number - numberok;
+	cout << a << "/" << b << " - " << c << "/" << d << " = " << x.get_num() << "/" << x.get_den() << endl;
+	x = number * numberok;
+	cout << a << "/" << b << " * " << c << "/" << d << " = " << x.get_num() << "/" << x.get_den() << endl;
+	x = number / numberok;
+	cout << a << "/" << b << " / " << c << "/" << d << " = " << x.get_num() << "/" << x.get_den() << endl;
+
+	++number;
+	x = number * numberok;
+	cout << "++" << a << "/" << b << " * " << c << "/" << d << " = " << x.get_num() << "/" << x.get_den() << endl;
+	cout << "Значение дроби 1 = " << number.get_num() << "/" << number.get_den() << endl;
+	
+	cout << number.get_num() << "/" << number.get_den() << "--" << " * " << c << "/" << d << " = ";
+	x = number-- * numberok;
+	cout << x.get_num() << "/" << x.get_den() << endl;
+	cout << "Значение дроби 2 = " << number.get_num() << "/" << number.get_den() << endl;
+	return 0;
+}
