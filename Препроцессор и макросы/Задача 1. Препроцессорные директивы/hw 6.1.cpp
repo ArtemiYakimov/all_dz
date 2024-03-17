98a77883d9f8e76d5fa2474a0a78c57f557d5d17
Artemi Yakimov <artemiyakimov246@gmail.com>
2024-03-17 11:26:31 +1100

    update_2

---
+++ Препроцессор и макросы/Задача 1. Препроцессорные директивы/hw 6.1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <Windows.h>
+#define MODE 1
+
+using namespace std;
+
+#if MODE == 1
+    void add() {
+        int a, b;
+        cout << "Работаю в боевом режиме" << endl;
+        cout << "Введите число 1: ";
+        cin >> a;
+        cout << "Введите число 2: ";
+        cin >> b;
+        cout << "Результат сложения: " << (a + b);
+     }
+#endif
+
+#ifdef MODE
+    int main() {
+        SetConsoleCP(1251);
+        SetConsoleOutputCP(1251);
+        #if MODE == 0
+            cout << "Работаю в режиме тренировки" << endl;
+        #elif MODE == 1
+            add();
+        #else
+            cout << "Неизвестный режим. Завершение работы" << endl;
+        #endif
+        return 0;
+    }
+#endif 
