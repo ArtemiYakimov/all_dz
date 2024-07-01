8771157227e5f4d9f870df4b98536e229154fb3e
Artemi Yakimov <artemiyakimov246@gmail.com>
2024-07-01 17:18:46 +1100

    семантика

---
+++ семантика/задание 1/ConsoleApplication1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <vector>
+#include <string>
+
+// Шаблонная функция для перемещения содержимого одного вектор в другой
+template <typename T>
+void переместитьВекторы(std::vector<T>& src, std::vector<T>& dst) {
+    dst = std::move(src);
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    // Создаем два вектора строк
+    std::vector<std::string> один = { "строка_тест1", "строка_тест2" };
+    std::vector<std::string> два;
+
+    // Выводим содержимое векторов до перемещения
+    std::cout << "Содержимое вектора один до перемещения:" << std::endl;
+    for (const auto& str : один) {
+        std::cout << str << std::endl;
+    }
+    std::cout << "Содержимое вектора два до перемещения:" << std::endl;
+    for (const auto& str : два) {
+        std::cout << str << std::endl;
+    }
+
+    // Перемещаем содержимое вектора один в вектор два
+    переместитьВекторы(один, два);
+
+    // Выводим содержимое векторов после перемещения
+    std::cout << "Содержимое вектора один после перемещения:" << std::endl;
+    for (const auto& str : один) {
+        std::cout << str << std::endl;
+    }
+    std::cout << "Содержимое вектора два после перемещения:" << std::endl;
+    for (const auto& str : два) {
+        std::cout << str << std::endl;
+    }
+
+    return 0;
+}
---
+++ семантика/задание 2/ConsoleApplication1.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+class big_integer {
+private:
+    std::string value;
+
+public:
+    big_integer() : value("") {} // default constructor
+    big_integer(const std::string& str) : value(str) {
+        while (!value.empty() && value[0] == '0') {
+            value.erase(0, 1);
+        }
+    }
+
+    big_integer(big_integer&& other) noexcept : value(std::move(other.value)) {}
+
+    big_integer& operator=(big_integer&& other) noexcept {
+        if (this != &other) {
+            value = std::move(other.value);
+        }
+        return *this;
+    }
+
+    big_integer operator+(const big_integer& other) const {
+        big_integer result;
+        result.value = add(value, other.value);
+        return result;
+    }
+
+    big_integer operator*(int multiplier) const {
+        big_integer result;
+        result.value = multiply(value, multiplier);
+        return result;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const big_integer& num) {
+        os << num.value;
+        return os;
+    }
+
+private:
+    static std::string add(const std::string& a, const std::string& b) {
+        std::string result;
+        int carry = 0;
+        size_t i = a.size() - 1; // changed to size_t
+        size_t j = b.size() - 1; // changed to size_t
+
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a[i--] - '0';
+            }
+            if (j >= 0) {
+                sum += b[j--] - '0';
+            }
+            carry = sum / 10;
+            result.push_back('0' + sum % 10);
+        }
+
+        std::reverse(result.begin(), result.end());
+        return result;
+    }
+
+    static std::string multiply(const std::string& num, int multiplier) {
+        std::string result;
+        int carry = 0;
+
+        for (size_t i = num.size() - 1; i >= 0; --i) { // changed to size_t
+            int product = (num[i] - '0') * multiplier + carry;
+            carry = product / 10;
+            result.push_back('0' + product % 10);
+        }
+
+        if (carry) {
+            result.push_back('0' + carry);
+        }
+
+        std::reverse(result.begin(), result.end());
+        return result;
+    }
+};
+
+int main() {
+    big_integer number1("114575");
+    big_integer number2("78524");
+    big_integer result = number1 + number2;
+    std::cout << result << std::endl; // 193099
+
+    return 0;
+}
