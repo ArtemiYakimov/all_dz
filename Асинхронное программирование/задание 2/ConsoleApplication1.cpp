d3bccc9be877565867bd9c6d2f352b17b7dcda2e
Artemi <Artemiyakimov246@gamil.com>
2024-08-23 14:21:13 +1100

    dz исправления

--- Асинхронное программирование/задание 2/ConsoleApplication1.cpp
+++ Асинхронное программирование/задание 2/ConsoleApplication1.cpp
@@ -6,18 +6,22 @@
 #include <numeric>
 
 template <typename InputIterator, typename Function>
-void parallel_for_each(InputIterator first, InputIterator last, Function&& f) {
+void parallel_for_each(InputIterator first, InputIterator last, Function&& f, size_t min_chunk_size = 1) {
     const size_t length = std::distance(first, last);
+    if (length <= min_chunk_size) {
+        std::for_each(first, last, std::forward<Function>(f));
+        return;
+    }
+
     const size_t num_threads = std::thread::hardware_concurrency();
-    const size_t chunk_size = length / num_threads;
+    const size_t chunk_size = (length + num_threads - 1) / num_threads;
 
     std::vector<std::future<void>> futures;
 
     for (size_t i = 0; i < num_threads; ++i) {
         InputIterator start = first + i * chunk_size;
         InputIterator end = (i == num_threads - 1) ? last : first + (i + 1) * chunk_size;
-
-        futures.emplace_back(std::async(std::launch::async, [start, end, &f]() {
+        futures.emplace_back(std::async(std::launch::async, [start, end, f = std::forward<Function>(f)]() {
             std::for_each(start, end, f);
             }));
     }
