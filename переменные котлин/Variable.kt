5ddd213d47a80f8f789ce0a4b7636a129d21874b
Artemi <Artemiyakimov246@gamil.com>
2024-10-11 19:56:56 +1100

    дз

---
+++ переменные котлин/Variable.kt
@@ -0,0 +1,8 @@
+fun main () {
+    val age: Int = 18;
+    val name: String = "Артемий";
+    val isStudent: Boolean = true;
+    val salary: Double = 2000.25
+
+    println("Age: $age | Name: $name | Is Student: $isStudent | Salary: $salary")
+}
